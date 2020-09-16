f1482eb921a5fd89412a79d935676c1c4e476b98
Marissa <marissabellusci@gmail.com>
2020-09-16 10:15:04 -0600

    updated control panel and added embedded twitter profiles for personal/flotrack pages

---
+++ parker-navarro-portfolio/src/components/ControlPanel.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class ControlPanel extends React.Component{
+    static propTypes = {
+       
+      };
+    
+      state = {
+        uid: null,
+        owner: null,
+      }
+
+      render(){
+          return(
+              <div id="control-panel">
+                    <label for="fish-edit" className="panel-header">Control Panel</label>
+                    <form className="fish-edit" onSubmit={this.createFish}>  
+                    <input name="name" ref={this.nameRef} type="text" placeholder="Name"/>
+                    <input name="price" ref={this.priceRef} type="text" placeholder="Price" />
+                    <select name="status" ref={this.statusRef}> 
+                        <option value="available">Fresh!</option>
+                        <option value="unavailable">Sold Out!</option>
+                     </select>
+      
+                    <textarea name="desc" ref={this.descRef} placeholder="Description"/>
+                    <input name="image" ref={this.imageRef} type="text" placeholder="Image"/>
+                    <button type="submit">+ Add Text</button>
+          </form>
+
+              </div>
+          )
+      }
+
+}
+
+export default ControlPanel;
--- parker-navarro-portfolio/src/components/Social.js
+++ parker-navarro-portfolio/src/components/Social.js
@@ -9,7 +9,10 @@ class Social extends React.Component{
         return(
             <div id="social-media">
             <h2 className="social-media-header">Social Media</h2>
-            <p className="social-media-para"></p>
+            <p className="social-media-para">
+            <a class="twitter-timeline" data-lang="en" href="https://twitter.com/ParkerANavarro?ref_src=twsrc%5Etfw">Tweets by @ParkerANavarro</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <a class="twitter-timeline" data-lang="en" href="https://twitter.com/flosports?ref_src=twsrc%5Etfw">Tweets by flosports</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </p>
             </div>
         )
     }
